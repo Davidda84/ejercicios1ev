92d313a5761a4f48b7a9ed7ce837ea2c613c55ce
Davidda84 <davidda83@educastur.es>
2020-10-27 13:44:41 +0100

    Ejercicio 5 de la unidad 2 resuelto y mejorado

--- src/unidad2/Sueldo.java
+++ src/unidad2/Sueldo.java
@@ -11,22 +11,23 @@ public class Sueldo {
 			comisiones. Se utilizará el teclado para la entrada de datos y la consola para la
 			salida. Para resolver este problema está permitido utilizar la clase Scanner.*/
 
-		Scanner teclado = new Scanner(System.in);
-		double sueldoBase;
-		double venta1;
-		double venta2;
-		double venta3;
-		double sueldoMes;
-		System.out.println("Introduzca su sueldo Base:");
-		sueldoBase = teclado.nextDouble();
-		System.out.println("Introduzca el importe de la primera venta:");
-		venta1 = teclado.nextDouble();
-		System.out.println("Introduzca el importe de la segunda venta:");
-		venta2 = teclado.nextDouble();
-		System.out.println("Introduzca el importe de la tercera venta:");
-		venta3 = teclado.nextDouble();
-		sueldoMes = sueldoBase + (venta1 + venta2 + venta3) * 0.1;
-		System.out.printf("Sueldo a final de mes será %.2f€",sueldoMes);
+		try (Scanner teclado = new Scanner(System.in)) {
+			double sueldoBase;
+			double venta1;
+			double venta2;
+			double venta3;
+			double sueldoMes;
+			System.out.println("Introduzca su sueldo Base:");
+			sueldoBase = teclado.nextDouble();
+			System.out.println("Introduzca el importe de la primera venta:");
+			venta1 = teclado.nextDouble();
+			System.out.println("Introduzca el importe de la segunda venta:");
+			venta2 = teclado.nextDouble();
+			System.out.println("Introduzca el importe de la tercera venta:");
+			venta3 = teclado.nextDouble();
+			sueldoMes = sueldoBase + (venta1 + venta2 + venta3) * 0.1;
+			System.out.printf("Sueldo a final de mes será %.2f€",sueldoMes);
+		}
 		
 
 	}
